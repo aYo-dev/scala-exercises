e427fa5c8e9f9ae202b9694caa392f441910fe17
Asen Yovchev <ayovcheff@gmail.com>
2018-12-18 13:14:38 +0200

    implement the terrain and block methods

--- streams/src/main/scala/streams/StringParserTerrain.scala
+++ streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,12 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = (position: Pos) => {
+    levelVector(position.row)(position.col) match {
+      case '-' => false
+      case _ => true
+    }
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +67,14 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    for {
+      row <- levelVector
+      rowIndex <- levelVector indexOf row
+      if row contains c
+      colIndex <- row.indexWhere(el => el == c)
+    } yield Pos(rowIndex, colIndex)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
